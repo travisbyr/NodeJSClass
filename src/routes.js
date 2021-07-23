7eb9aad9019c034ae590e19993ed9193f548dbe6
TravisByr <travis-byrman@live.nmit.ac.nz>
2021-07-23 13:58:21 +1200

    added code

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const requestHandler = (req, res) => {
+
     const url = req.url
     const method = req.method
 
@@ -15,10 +16,25 @@ const requestHandler = (req, res) => {
 
     if (url === '/message' && method === 'POST') {
         const body = []
-        req.on('data', chunk => body.push(chunk))
-    }
-    return req.on('end'), () => { 
-        const paserBody = Buffer.concat(body).toString()
-        const message = paserBody.trim()
+        req.on('data', (chunk) =>  {
+            body.push(chunk)
+        });
+        return req.on('end', () => { 
+            const paserBody = Buffer.concat(body).toString()
+            const message = paserBody.split('=')[1]
+            fs.writeFile('./messages.txt', message, err => {
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            });
+        });
     }
 }
+
+    // res.setHeader('content-type', 'text/html')
+    // res.write('<head><tile>WEB601</tile></head>')
+    // res.write('<body><h1>WEB601</h1></body>')
+    // res.write('</html>')
+    // res.end()
+
+exports.handler = requestHandler 
